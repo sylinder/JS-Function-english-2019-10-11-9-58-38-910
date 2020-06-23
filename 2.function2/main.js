bc5c667257f0791b49a913c8f0f1eb5e707d09b8
袁柱 <yzsylinder@163.com>
2020-06-23 17:15:31 +0800

    Update main.js

--- 2.function2/main.js
+++ 2.function2/main.js
@@ -1,1 +1,13 @@
+function palindrome(message) {
+    var len = message.length;
+    var half_len = Math.floor(len / 2);
+    for (var i = 0; i < half_len; i++) {
+        if (message.charAt(i) != message.charAt(len - 1 - i)) {
+            return false;
+        }
+    }
+    return true;
+}
 
+console.log(palindrome("hello"));
+console.log(palindrome("abcba"));
